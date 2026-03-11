2e2f5e2cc31f2e96308a85568f0603ce3c7ffd26
hasnainali-creator <hasnaj827ali@gmail.com>
2026-03-11 17:00:03 +0500

    speed boost

--- astro.config.mjs
+++ astro.config.mjs
@@ -69,6 +69,17 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+          passes: 2,
+        },
+        mangle: true,
+      },
+    },
     server: {
       watch: {
         // Only ignore the output file, don't ignore content!
